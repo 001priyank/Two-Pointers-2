9c84d40dc071ec7b670dae5dc4cfae7d9bdf5b38
Priyank Singh <priyank.singh@sympli.com.au>
2022-01-07 14:24:43 +1100

    fix

--- mergeSorted.cpp
+++ mergeSorted.cpp
@@ -2,10 +2,10 @@ class Solution {
 public:
     void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
         
-        int low=0,highFirst = m-1, highOfFirstEnd=m+n-1;
+        int highFirst = m-1, highOfFirstEnd=m+n-1;
         int highSecond = n-1;
         
-        while(highOfFirstEnd>0 && (highSecond>=0 && highFirst>=0)){
+        while(highFirst>=0 && highSecond>=0){
             cout<<highFirst<< " "<<highOfFirstEnd<< " "<<highSecond<< endl;
             
             if(nums1[highFirst] < nums2[highSecond]) {
@@ -19,18 +19,13 @@ public:
                 highFirst--;
             }
             highOfFirstEnd--;
-            for(auto it:nums1){
-                cout<<it<< " ";
-            }
-            cout<<endl;
         }
-        if(highFirst == -1 && m>=0 ){
-            int i=0;
-            while(i<n){
-                nums1[i] = nums2[i];
-                i++;
-            } 
-        }
-        
+       
+        while(highSecond>=0){
+            nums1[highOfFirstEnd] = nums2[highSecond];
+            highOfFirstEnd--;
+            highSecond--;
+            
+        }        
     }
 };
